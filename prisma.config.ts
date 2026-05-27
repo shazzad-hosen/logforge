ff033431889571eb548ba14c9f67f6211dfaa0f0
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-05-27 20:28:57 +0600

    feat(projects): configure prisma and test it

---
+++ prisma.config.ts
@@ -0,0 +1,12 @@
+import { ENV } from "./src/config/env";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: ENV.DATABASE_URL,
+  },
+});
--- src/config/env.ts
+++ src/config/env.ts
@@ -6,6 +6,7 @@ dotenv.config({ quiet: true });
 const envSchema = z.object({
   PORT: z.string(),
   NODE_ENV: z.enum(["development", "production", "test"]),
+  DATABASE_URL: z.url(),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
@@ -20,4 +21,5 @@ if (!parsedEnv.success) {
 export const ENV = {
   PORT: Number(parsedEnv.data.PORT),
   NODE_ENV: parsedEnv.data.NODE_ENV,
+  DATABASE_URL: parsedEnv.data.DATABASE_URL,
 };
