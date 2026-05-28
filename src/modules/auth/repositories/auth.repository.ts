7c3546f2f4ce1aaacd93c0e76461facad4080fd9
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-05-28 19:53:12 +0600

    feat(auth): setup auth schema and auth repository

---
+++ src/modules/auth/repositories/auth.repository.ts
@@ -0,0 +1,18 @@
+import { prisma } from "../../../plugins/prisma.ts";
+
+export const findUserByEmail = async (email: string) => {
+  return prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+};
+
+export const createUser = async (email: string, password: string) => {
+  return prisma.user.create({
+    data: {
+      email,
+      password,
+    },
+  });
+};
