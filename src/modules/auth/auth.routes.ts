b4bb2c1df6b7890cfde86327f31fd16f7243f27d
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-06-03 12:41:49 +0600

    refactor(auth): refactor auth route and connect /refresh end point

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,12 +1,29 @@
 import { FastifyInstance } from "fastify";
+import authenticateRefreshSession from "./middlewares/authenticateRefreshSession.middleware.ts";
 
 import {
   registerUserController,
   loginUserController,
+  refreshUserTokenController,
 } from "./auth.controller.ts";
 
 export const authRoutes = async (app: FastifyInstance) => {
-  app.post("/register", registerUserController);
+  app.route({
+    method: "POST",
+    url: "/register",
+    handler: registerUserController,
+  });
 
-  app.post("/login", loginUserController);
+  app.route({
+    method: "POST",
+    url: "/login",
+    handler: loginUserController,
+  });
+
+  app.route({
+    method: "POST",
+    url: "/refresh",
+    preHandler: authenticateRefreshSession,
+    handler: refreshUserTokenController,
+  });
 };
