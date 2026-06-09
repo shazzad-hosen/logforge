83a6daf3305f7a8a8be689f625e6a0688a6104cb
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-06-09 22:01:24 +0600

    feat(auth): write, authenticate access token middleware

---
+++ src/modules/auth/middlewares/authenticateAccessToken.middleware.ts
@@ -0,0 +1,41 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import ApiError from "../../../utils/ApiError.ts";
+import { decodeAccessToken } from "../utils/generateTokens.ts";
+import { findUserById } from "../repositories/auth.repository.ts";
+
+declare module "fastify" {
+  interface FastifyRequest {
+    user?: object;
+  }
+}
+
+export const authenticateAccessToken = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
+  const authHeader = request.headers?.authorization as string;
+
+  if (!authHeader || authHeader.startsWith("Bearer ")) {
+    throw new ApiError(401, "Not authorized, invalid token format");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    throw new ApiError(401, "Not authorized, token missing");
+  }
+
+  try {
+    const decoded = decodeAccessToken(token);
+
+    const user = findUserById(decoded.sub);
+
+    if (!user) {
+      throw new ApiError(401, "User no longer exists");
+    }
+
+    request.user = user;
+  } catch (error) {
+    throw new ApiError(401, "Invalid or expired token");
+  }
+};
