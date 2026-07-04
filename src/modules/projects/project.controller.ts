aa8147d505a9e0ec07c4b77e7b8f40c08b2b89d3
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-07-04 13:01:40 +0600

    feat(project): write post /project controller

---
+++ src/modules/projects/project.controller.ts
@@ -0,0 +1,38 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { createProjectSchema } from "./project.schema.ts";
+import { createProject } from "./project.service.ts";
+
+interface AuthenticatedUser {
+  id: string;
+  email: string;
+  role: "user" | "admin";
+  isVerified: boolean;
+}
+
+export const createProjectController = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
+  const user = request.user as AuthenticatedUser;
+
+  const parsedData = createProjectSchema.safeParse(request.body);
+
+  if (!parsedData.success) {
+    const errors = Object.fromEntries(
+      parsedData.error.issues.map((i) => [i.path[0], i.message]),
+    );
+    return reply.status(400).send({ errors });
+  }
+
+  const { name, description } = parsedData.data;
+
+  const project = await createProject(user.id, name, description);
+
+  return reply.status(201).send({
+    success: true,
+    message: "Project created successfully",
+    project: {
+      
+    }
+  });
+};
